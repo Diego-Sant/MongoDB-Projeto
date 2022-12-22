062525c831ff6e26503a5fefebbb3173e35af918
Diego-Sant <diego_santana@unigranrio.br>
2022-12-22 19:47:38 -0300

    Referencing post to user

--- src/main/java/com/projetocurso/mongodbprojeto/domain/User.java
+++ src/main/java/com/projetocurso/mongodbprojeto/domain/User.java
@@ -1,9 +1,12 @@
 package com.projetocurso.mongodbprojeto.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document //(collection="user") caso queira deixar o nome diferente da classe
@@ -15,6 +18,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	// lazy = true significa que os dados só serão carregados quando acionados para evitar excesso de informações
+	@DBRef(lazy = true) // Referenciar uma coleção
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -48,6 +55,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
