d83d34e1f72b1fbb5f5d5a6e545ad929b8a25009
Diego-Sant <diego_santana@unigranrio.br>
2022-12-22 15:02:06 -0300

    Maven project

---
+++ src/main/java/com/projetocurso/mongodbprojeto/MongoDbProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.projetocurso.mongodbprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetocurso/mongodbprojeto/MongoDbProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocurso.mongodbprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
